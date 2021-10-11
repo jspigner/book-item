b612002d775cb1301acf2530ed50622a2d78d2ee
Julie Spigner <77714712+jspigner@users.noreply.github.com>
2021-10-10 20:37:05 -0700

    Create book-item.py

---
+++ book-item.py
@@ -0,0 +1,11 @@
+# Create a class called Book 
+# Add variables (title, pages, number, genre) 
+# Add print statements about the book item with formatted strings 
+
+class Book:
+  title = "Harry Potter and the Deathly Hallows"
+  pages = 607
+  number = "7th"
+  genre = "fantasy"
+  print(f"{title} has {pages} pages")
+  print(f"It is the {number} in the series.")
